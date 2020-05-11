e536b70c4b5ef0bd23e5bcbc3aa82ce8e49bee7f
Sjukdom <danielpy108@gmail.com>
2020-05-11 12:30:36 -0500

    The signup app is added

--- note_takin/note_takin/settings.py
+++ note_takin/note_takin/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'signup.apps.SignupConfig',
     'homepage.apps.HomepageConfig',
     'notes.apps.NotesConfig',
     'api.apps.ApiConfig',
